dacf79829dfc25ae44527d2b1ebbd912289bf205
Hector Torres <161650015+thector01@users.noreply.github.com>
2024-08-15 12:58:12 -0400

    Create sort_algorithms.py

---
+++ sort_algorithms.py
@@ -0,0 +1,117 @@
+import time
+import random
+
+class SortAlgorithms:
+    def bubble_sort(self, arr):
+        n = len(arr)
+        for i in range(n):
+            for j in range(0, n-i-1):
+                if arr[j] > arr[j+1]:
+                    arr[j], arr[j+1] = arr[j+1], arr[j]
+        return arr
+
+    def selection_sort(self, arr):
+        n = len(arr)
+        for i in range(n):
+            min_idx = i
+            for j in range(i+1, n):
+                if arr[j] < arr[min_idx]:
+                    min_idx = j
+            arr[i], arr[min_idx] = arr[min_idx], arr[i]
+        return arr
+
+    def insertion_sort(self, arr):
+        for i in range(1, len(arr)):
+            key = arr[i]
+            j = i - 1
+            while j >= 0 and key < arr[j]:
+                arr[j + 1] = arr[j]
+                j -= 1
+            arr[j + 1] = key
+        return arr
+
+    def quick_sort(self, arr):
+        if len(arr) <= 1:
+            return arr
+        pivot = arr[len(arr) // 2]
+        left = [x for x in arr if x < pivot]
+        middle = [x for x in arr if x == pivot]
+        right = [x for x in arr if x > pivot]
+        return self.quick_sort(left) + middle + self.quick_sort(right)
+
+    def merge_sort(self, arr):
+        if len(arr) <= 1:
+            return arr
+        mid = len(arr) // 2
+        left = self.merge_sort(arr[:mid])
+        right = self.merge_sort(arr[mid:])
+        return self.merge(left, right)
+
+    def merge(self, left, right):
+        result = []
+        i = j = 0
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                result.append(left[i])
+                i += 1
+            else:
+                result.append(right[j])
+                j += 1
+        result.extend(left[i:])
+        result.extend(right[j:])
+        return result
+
+    def bucket_sort(self, arr):
+        if len(arr) == 0:
+            return arr
+
+        max_value = max(arr)
+        size = max_value / len(arr)
+
+        buckets = [[] for _ in range(len(arr))]
+
+        for i in range(len(arr)):
+            j = int(arr[i] / size)
+            if j != len(arr):
+                buckets[j].append(arr[i])
+            else:
+                buckets[len(arr) - 1].append(arr[i])
+
+        result = []
+        for bucket in buckets:
+            result.extend(self.insertion_sort(bucket))
+
+        return result
+
+    def heap_sort(self, arr):
+        n = len(arr)
+        for i in range(n // 2 - 1, -1, -1):
+            self.heapify(arr, n, i)
+        for i in range(n - 1, 0, -1):
+            arr[i], arr[0] = arr[0], arr[i]
+            self.heapify(arr, i, 0)
+        return arr
+
+    def heapify(self, arr, n, i):
+        largest = i
+        left = 2 * i + 1
+        right = 2 * i + 2
+
+        if left < n and arr[left] > arr[largest]:
+            largest = left
+
+        if right < n and arr[right] > arr[largest]:
+            largest = right
+
+        if largest != i:
+            arr[i], arr[largest] = arr[largest], arr[i]
+            self.heapify(arr, n, largest)
+
+def generate_random_list(size, lower_bound=0, upper_bound=10000):
+    return [random.randint(lower_bound, upper_bound) for _ in range(size)]
+
+def time_algorithm(algorithm, data):
+    start_time = time.time()
+    algorithm(data.copy())
+    end_time = time.time()
+    return end_time - start_time
